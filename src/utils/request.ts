da98c22e37c710abde9fe90a0eba5440a230fbfe
867406308 <32955236+867406308@users.noreply.github.com>
2023-04-26 20:36:18 +0800

    修改api地址

--- src/utils/request.ts
+++ src/utils/request.ts
@@ -5,7 +5,7 @@ import router from '@/router';
 import { removeToken, getToken } from '@/utils/token';
 
 const instance = axios.create({
-  baseURL: 'http://39.101.195.100:18085/api',
+  baseURL: 'http://39.101.195.100:8085/api',
   timeout: 6000,
   headers: {
     'Content-Type': 'application/json;charset=utf-8',
