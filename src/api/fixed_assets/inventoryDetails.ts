ff10e29bba4169a273d69c6c48f7f529e72a4729
704347074@qq.com <704347074@qq.com>
2024-10-28 09:15:21 +0800

    12

--- src/api/fixed_assets/inventoryDetails.ts
+++
@@ -1,9 +0,0 @@
-import request from '@/utils/request';
-
-export function page(params: any) {
-  return request({
-    url: '/fixedAssets/assets/inventory/details/page',
-    method: 'get',
-    params,
-  });
-}
